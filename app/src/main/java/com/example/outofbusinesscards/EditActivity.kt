082e2bf00ad8878f0411c2879aa395c7e56cb9f0
Administrator <admin@example.com>
2020-04-25 16:35:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/outofbusinesscards/EditActivity.kt
@@ -0,0 +1,59 @@
+package com.example.outofbusinesscards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_edit.*
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+        PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val company = getString("company", "")
+            val postal = getString("postal", "")
+            val address = getString("address", "")
+            val tel = getString("tel", "")
+            val fax = getString("fax", "")
+            val email = getString("email", "")
+            val url = getString("url", "")
+            val position = getString("position", "")
+            val name = getString("name", "")
+
+            companyEdit.setText(company)
+            postalEdit.setText(postal)
+            addressEdit.setText(address)
+            telEdit.setText(tel)
+            faxEdit.setText(fax)
+            emailEdit.setText(email)
+            urlEdit.setText(url)
+            positionEdit.setText(position)
+            nameEdit.setText(name)
+        }
+
+        saveBtn.setOnClickListener {
+            saveData()
+            finish()
+        }
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun saveData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putString("company", companyEdit.text.toString())
+                .putString("postal", postalEdit.text.toString())
+                .putString("address", addressEdit.text.toString())
+                .putString("tel", telEdit.text.toString())
+                .putString("fax", faxEdit.text.toString())
+                .putString("email", emailEdit.text.toString())
+                .putString("url", urlEdit.text.toString())
+                .putString("position", positionEdit.text.toString())
+                .putString("name", nameEdit.text.toString())
+                .apply()
+    }
+}
